ab96daa96e6a9fc05ff95d9eb93e5f17a9b5dea9
CharmingChocobo <jbeenen93@gmail.com>
2025-03-03 16:47:53 +0100

    Try python example (see url in code comments)

---
+++ scripts/main.py
@@ -0,0 +1,66 @@
+# IMPORTS
+# import requests
+import json
+# import sys
+import support_module as S
+
+# Load configuration file
+with open('config.json', 'r', encoding="utf-8") as stream:
+    config = json.load(stream)
+
+# Connect to API
+creds = S.get_credentials(config['path_to_credentials']) # NOTE: ATM just "path_to_credentials": "path/to/credentials"
+
+# Start session TODO: this would be nicer in a separate function.
+# session = requests.Session()
+# base_url = creds["ADDRESS"]
+# session.headers['referer'] = base_url.removesuffix("api/mlquery/")
+# session.get(base_url + "start/")
+# payload = {
+#     'apikey': creds["APIKEY"],
+#     'csrfmiddlewaretoken': session.cookies.get_dict()['csrftoken']
+# }
+session, payload = S.start_session(creds)
+
+
+# Example (https://apimlqv2.tenwiseservice.nl/html/pythonexample.html)
+### Get the relations for 2 concepts ###
+# In this case we look at relations with
+# CXCR3 (HGNC:4540) and Parkinsons Disease (TWDIS_06685)
+# These are referred to as the subjects.
+
+payload['concept_ids_subject'] = "HGNC:4540,TWDIS_06685"
+results = session.post(
+    creds["ADDRESS"] + "conceptset/relations/",
+    payload
+)
+
+js = results.json()
+relations = js['result']['relations']
+
+### Get all ids and names for which a relation is found ###
+# These are referred to as the "object"
+# This is done by the annotation method
+
+object_ids = {x['object']:1 for x in relations}
+ids = ["HGNC:4540","TWDIS_06685"] + list(object_ids.keys())
+payload['concept_ids'] = ",".join(ids)
+results = session.post(creds["ADDRESS"] + "conceptset/annotation/",
+                       payload)
+js = results.json()
+annotation = js['result']['annotation']
+
+### Print out some details for each relation ###
+print("\t".join(['Subject','Object','Score','Overlap']))
+
+for relation in relations:
+    if relation['score'] >100:
+        if relation['overlap'] > 10:
+            print("\t".join([
+                annotation[relation['subject']]['name'][0],
+                annotation[relation['object']]['name'][0],
+                str(relation['score']),
+                str(relation['overlap'])
+              ]
+            )
+)
---
+++ scripts/support_module.py
@@ -0,0 +1,59 @@
+# MODULE
+"""
+support_module.py: A collection of supporting functions.
+
+INITIALISATION
+* get_credentials
+"""
+# IMPORTS
+import requests
+import sys
+
+# FUNCTIONS
+# Get credentials
+def get_credentials(path_to_text: str) -> dict:
+    """
+    Loads a file (path_to_text) with tab separated values and creates/returns a dictionary. 
+    The first column will be the key, second column will be the value.
+    """
+    try:
+        with open(path_to_text) as file:
+            lines = file.readlines()
+            list_credentials = [l.strip('\n').split('\t') for l in lines]
+            dict_credentials = {l[0]: l[1] for l in list_credentials}
+    except FileNotFoundError:
+        print("ERROR: Entered file not found (maybe there's a typo?): ", path_to_text)
+        sys.exit()
+    print("Loaded credentials")
+    return dict_credentials
+
+# Start session and get a payload
+def start_session(login_credentials: dict):
+    """
+    Start a session with requests.Sessions() using the provided login_credentials, returns the session and a template payload.
+    
+    Args:
+        login_credentials (dict): Must contain following keys: 'APIKEY', 'ADDRESS'.
+
+    Returns:
+        session (requests): ???
+        payload (dict): A payload template ???
+    """
+    session = requests.Session()
+    login_credentials["ADDRESS"]
+    session.headers['referer'] = login_credentials["ADDRESS"].removesuffix("api/mlquery/") # NOTE: 'referer' is not a typo. Please ignore cSpell.
+    session.get(login_credentials["ADDRESS"] + "start/")
+    payload = {
+        'apikey': login_credentials["APIKEY"],
+        'csrfmiddlewaretoken': session.cookies.get_dict()['csrftoken']
+    }
+
+    return session, payload
+
+
+
+# APPENDIX
+if __name__ == "__main__":
+    print(__doc__)
+else:
+    print(f"Custom module '{__name__}' is imported successfully!")
