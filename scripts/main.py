cd905f1109a45a7fc472d53b1be733f0b727d826
CharmingChocobo <jbeenen93@gmail.com>
2025-03-07 17:38:39 +0100

    Retrieved PMIDs from (method 1) free_search and (method 2) /evidence/ or /hits/. (Concept_ids of pesticides are still missing!)

--- scripts/main.py
+++ scripts/main.py
@@ -1,8 +1,16 @@
 """
 main.py
 
-Please find documentation about TenWise Knowledge Map API here:
+Please find documentation about TenWise Knowledge Map API, which is used in this script, here:
 https://apimlqv2.tenwiseservice.nl/
+
+Get full text from keywords
+- Method 1: Select PMIDs from the TenWise PubMed 'MEDLINE abstracts' database by free search (not limited to concepts in the TenWise'Vocabulary' database).
+- Method 2: Retrieve concept_ids of keywords from TenWise 'Vocabulary' database 
+and then select PMIDs from the TenWise PubMed 'MEDLINE abstracts' database.
+- Query TenWise PubMed database for full text of selected PMIDs.
+1) vanuit keywords in `free_search`, of 2) vanuit concept_ids uit de 'Vocabularies',
+
 """
 
 # IMPORTS
@@ -16,11 +24,25 @@ with open('config.json', 'r', encoding="utf-8") as stream:
 # Connect to API
 creds = F.get_credentials(config['path_to_credentials'])
 
-# Start session
+# Start session #TODO: Create a class for the session?
 session, payload = F.start_session(creds)
 
-# TODO: Get full text from keywords 'pesticides' and 'Parkinsons disease'
-# How to retrieve concept_ids? (Parkinsons Disease (TWDIS_06685))
+### Get full text from keywords 'pesticides' and 'Parkinsons disease'
+### PUBMED USING FREE SEARCH (TODO: 'define free_search')
+payload['terms'] = '(pesticide OR pesticides) AND parkinson'
+results = session.post(
+    creds["ADDRESS"] + "refset/free_search",
+    payload
+)
+
+js = results.json()
+result = js['result']
+# > 'hitnr': 126
+# NOTE: Ready to retrieve full text?
+
+
+
+### PUBMED USING KMAP
 # Get all Parkinsons Disease concept_ids
 payload['terms'] = "Parkinson"
 payload['wildcard'] = 'true'
@@ -29,36 +51,29 @@ results = session.post(
     creds["ADDRESS"] + "concept/search/",
     payload
 )
+payload['wildcard'] = 'false' # <- Turn off wildcard (cleaning after myself)
 
 js = results.json()
 hits = list(js['result']['hits'].keys())
 # NOTE: Disease concepts start with "TWDIS", therefore hits are filtered on "TWDIS": https://apimlqv2.tenwiseservice.nl/html/all_help.html#vocabularies
 park_hits = [h for h in hits if h[:5] == 'TWDIS']
 
-# TODO: Get all pesticides concept_ids
-
-# NOTE: "Parkinsons Disease" id is not queried the same way CXCR3 is queried.. 
-# list_keywords = ["Parkinsons", "CXCR3"]
-# list_keywords = ["Cholesterol", "CXCR3"]
-# payload['terms'] = ",".join(list_keywords)
-
-# payload['terms'] = "CXCR3"
 
 
-# payload['terms'] = "CXCR"
-payload['wildcard'] = 'true'
-
+# TODO: Not sure if my conceptset/evidence/ output is correct.. (Think so!)
+# payload['concept_ids'] = 'TWDIS_17683'
+payload['concept_ids'] = ','.join(park_hits)
 results = session.post(
-    creds["ADDRESS"] + "concept/search/",
+    creds["ADDRESS"] + "conceptset/evidence/",
+    # creds["ADDRESS"] + "conceptset/hits/",
     payload
 )
-
 js = results.json()
-hits = list(js['result']['hits'].keys())
+hits = list(js['result']['hits'])
 
+# NOTE: (Once pesticides are added) Ready to retrieve full text?
 
-
-
+###
 
 #############################################################################
 ####### Example (https://apimlqv2.tenwiseservice.nl/html/pythonexample.html)
