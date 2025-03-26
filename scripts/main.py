f48defa6d721ebd55fa52f241656d7cf268d42f0
CharmingChocobo <jbeenen93@gmail.com>
2025-03-26 16:56:22 +0100

    Retrieved PMIDs now include pesticides in method 2 (/hits/ was removed since it did returns and error if input is >500 concept_ids)

--- scripts/main.py
+++ scripts/main.py
@@ -16,6 +16,7 @@ and then select PMIDs from the TenWise PubMed 'MEDLINE abstracts' database.
 # IMPORTS
 import json
 import functions as F
+import pandas as pd
 
 # Load configuration file
 with open('config.json', 'r', encoding="utf-8") as stream:
@@ -27,24 +28,28 @@ creds = F.get_credentials(config['path_to_credentials'])
 # Start session #TODO: Create a class for the session?
 session, payload = F.start_session(creds)
 
-### Get full text from keywords 'pesticides' and 'Parkinsons disease'
+### Get full text from keywords "pesticides" and "Parkinson's disease"
 ### PUBMED USING FREE SEARCH (TODO: 'define free_search')
-payload['terms'] = '(pesticide OR pesticides) AND parkinson'
+payload['terms'] = "(pesticide OR pesticides) AND parkinson's"
 results = session.post(
     creds["ADDRESS"] + "refset/free_search",
     payload
 )
 
 js = results.json()
-result = js['result']
-# > 'hitnr': 126
-# NOTE: Ready to retrieve full text?
+hits_on_free_search = js['result']['pmids']
 
+# js['result'] =
+# >  parkinson"  , 'hitnr': 126, 'pmids': ['32943485', '...']
+# >  parkinson's", 'hitnr': 866, 'pmids': ['37354828',
+# NOTE: I expected 'parkinson' to have more hits, since it would include "parkinson's"?
+# NOTE: Ready to retrieve full text?
 
 
 ### PUBMED USING KMAP
 # Get all Parkinsons Disease concept_ids
-payload['terms'] = "Parkinson"
+payload['terms'] = "parkinson"
+# payload['terms'] = "parkinson's" # NOTE: this returns an error: "You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near \'s\')\' at line 1"
 payload['wildcard'] = 'true'
 
 results = session.post(
@@ -56,21 +61,32 @@ payload['wildcard'] = 'false' # <- Turn off wildcard (cleaning after myself)
 js = results.json()
 hits = list(js['result']['hits'].keys())
 # NOTE: Disease concepts start with "TWDIS", therefore hits are filtered on "TWDIS": https://apimlqv2.tenwiseservice.nl/html/all_help.html#vocabularies
-park_hits = [h for h in hits if h[:5] == 'TWDIS']
+park_hits = [h for h in hits if h[:5] == 'TWDIS'] # ['TWDIS_03314', 'TWDIS_03315', ...]
 
 
+# TODO: Get all pesticide concept_ids
+# Current format is: `TWPHI_XXXXX \t name_pesticide`
+pest_hits = pd.read_csv(
+    config["path_to_pesticide_ids"],
+    header = None,
+    sep = "\t"
+    ).iloc[:,0].to_list()
 
 # TODO: Not sure if my conceptset/evidence/ output is correct.. (Think so!)
 # payload['concept_ids'] = 'TWDIS_17683'
-payload['concept_ids'] = ','.join(park_hits)
+payload['concept_ids'] = ",".join([*park_hits, *pest_hits])
 results = session.post(
     creds["ADDRESS"] + "conceptset/evidence/",
     # creds["ADDRESS"] + "conceptset/hits/",
     payload
 )
 js = results.json()
-hits = list(js['result']['hits'])
+hits_on_concept_ids = [d["pmid"]for d in js['result']['evidence']]
+# > ['3262231', ...] pmids
 
+
+# TODO: Where can I extract the PMC ids?
+# NOTE: Pesticides TW_ids: https://ironman.tenwisedev.nl/public/all_pesticide_concept_ids.txt
 # NOTE: (Once pesticides are added) Ready to retrieve full text?
 
 ###
