e47a7f26ed5897102b64f4443ddda691f60b69e3
CharmingChocobo <jbeenen93@gmail.com>
2025-03-28 16:07:20 +0100

    Done reading OpenAlex docs, ready to start coding

--- scripts/testing.py
+++ scripts/testing.py
@@ -1,5 +1,16 @@
 # Testing OpenAlex
 
+"""
+The limits are:
+
+max 100,000 calls every day, and also
+
+max 10 requests every second.
+
+We can raise the limit: as an academic researcher it's free. Just send an email to support@openalex.org
+https://docs.openalex.org/how-to-use-the-api/rate-limits-and-authentication
+"""
+
 import requests
 
 session = requests.Session()
@@ -10,14 +21,16 @@ session = requests.Session()
 pmid = '23042453'
 
 hits_on_concept_ids = [34429776, 6905769, 32625953, 21962330, 24370860, 25205153, 21309226, 585583, 39456132, 24370859, 18023468, 35076356, 32626398, 25035916, 22420260, 18343389, 25461413, 1783584, 39541787, 32625585, 25844860, 17474521, 8997741, 3796654, 38845614, 17679438, 15982717, 10775335, 20223083, 19913234, 15146917, 11884232, 37196509, 33864981, 30586803, 28600808, 25149231, 19165729, 16308868, 9129300, 34425306, 26915710, 25255562, 21936321, 21410986, 20183062, 12192909, 10868593, 32045778, 26466578]
-hits_on_concept_ids = hits_on_concept_ids[:10] # Cap!
+hits_on_concept_ids = [str(i) for i in hits_on_concept_ids]
+hits_on_concept_ids = hits_on_concept_ids[:3] # Cap!
 
-# url = f"https://api.openalex.org/works?filter=pmid:{','.join(pmid)}&mailto=j.beenen@pl.hanze.nl"
-# url = f"https://api.openalex.org/works?filter=pmid:{','.join(pmid)}"
-url = f"https://api.openalex.org/works?filter=ids.pmid:{pmid}&mailto=j.beenen@st.hanze.nl"
+# TODO: Refactor!
+# NOTE: Query up to 50 ids per request. (we could try more with `&per-page=200`) 200 is the max. per-page. https://blog.ourresearch.org/fetch-multiple-dois-in-one-openalex-api-request/
+# If you need more pages, use cursor paging https://docs.openalex.org/how-to-use-the-api/get-lists-of-entities/paging#cursor-paging
+url = f"https://api.openalex.org/works?filter=pmid:{'|'.join(hits_on_concept_ids)},best_open_version:acceptedOrPublished&select=best_oa_location,open_access&mailto=j.beenen@pl.hanze.nl"
+# url = f"https://api.openalex.org/works?filter=pmid:{'|'.join(hits_on_concept_ids)},best_open_version:acceptedOrPublished&mailto=j.beenen@pl.hanze.nl"
 
-
-# for 
+# &filter=best_open_version:acceptedOrPublished
 
 
 
@@ -26,7 +39,11 @@ results = response['results']
 
 print(results)
 # for paper in results, check if an oa_url is available.
-# NOTE: `oa_url` can refer to a html or a pdf! (-> `best_oa_location` > 'pdf_url' might be better!! (Maybe combine it with `is_accepted`? https://docs.openalex.org/api-entities/works/work-object/location-object#is_accepted !! ALSO SEE https://docs.openalex.org/api-entities/works/work-object/location-object#version))
+# NOTE: `oa_url` can refer to a html or a pdf! (-> `best_oa_location` > 'pdf_url' might be better!! But is not always available.. Then try to include `landing_page_url`)
+# (Maybe combine it with `is_accepted`? https://docs.openalex.org/api-entities/works/work-object/location-object#is_accepted !! ALSO SEE https://docs.openalex.org/api-entities/works/work-object/location-object#version))
+# (^^ this can be done in the API: https://docs.openalex.org/api-entities/works/filter-works#best_open_version)
+# `?filter=best_open_version:acceptedOrPublished`
+# `?select=best_oa_location`
 # EXAMPLE: https://pmc.ncbi.nlm.nih.gov/articles/PMC8370139/ or https://europepmc.org/backend/ptpmcrender.fcgi?accid=PMC3580294&blobtype=pdf 
 # Maybe this ^ can be solved by ending a url with 
 # 1) check if url ends with 'pdf',
