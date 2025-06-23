ca1dedde694bfe7acfb54a43c1a999f8fb34afb1
CharmingChocobo <jbeenen93@gmail.com>
2025-06-23 17:43:43 +0200

    Trying to implement SBERT transformer

--- scripts/corpus2vector.py
+++ scripts/corpus2vector.py
@@ -8,6 +8,25 @@
 import argparse
 from pathlib import Path
 import pandas as pd
+# import re
+import pickle
+
+
+from sentence_transformers import SentenceTransformer # SBERT
+# https://github.com/MartinoMensio/spacy-sentence-bert
+
+# universal-sentence-encoder This should be better than BERT? https://research.google/blog/advances-in-semantic-textual-similarity/
+# https://github.com/MartinoMensio/spacy-universal-sentence-encoder
+
+# bert roberta combi
+# https://colab.research.google.com/github/keras-team/keras-io/blob/master/examples/nlp/ipynb/sentence_embeddings_with_sbert.ipynb
+
+# pegasus
+
+# sent2vect met spacy
+# https://stackoverflow.com/questions/61133531/how-does-spacy-generate-vectors-for-phrases
+
+# https://codesphere.com/articles/best-open-source-sentence-embedding-models
 
 # FUNCTIONS
 def collect_arguments() -> argparse.Namespace:
@@ -21,8 +40,8 @@ def collect_arguments() -> argparse.Namespace:
         dest = "embedding_model",
         required = True,
         help = "Provide the name of the embedding model.",
-        choices = ["sent2vec", "bow", "tf-idf", "transformer"],
-        default = "sent2vec"
+        choices = ["sbert", "bow", "tf-idf", "transformer"],
+        default = "sbert"
     )
 
     parser.add_argument(
@@ -34,11 +53,24 @@ def collect_arguments() -> argparse.Namespace:
 
     return parser.parse_args()
 
+def preprocess_corpus_for_embedder(serie: pd.Series, replace_abbr : dict = None) -> list:
+    """Pre-process sentences (in pd.Series) to a list of sentences that is ready for an embedder for sentence similarity analysis. 
+    """
+    # Replace abbreviated words (TODO: is this necessary though?)
+    if isinstance(replace_abbr, dict):
+        for k in replace_abbr.keys():
+            # Replace `key` (e.g. "PD") with `value` (e.g. "parkinson disease").
+            serie = serie.str.replace(k, replace_abbr[k], case= True)
+    # Make all sentence_text lower case & remove puntuations & turn `serie` (pd.Series) into a list
+    list_sentences = serie.str.lower().str.strip('\[.*?\]').values
 
-def main():
-    """
-    Needs an docstring
-    """
+    return list_sentences
+
+
+# def main():
+#     """
+#     Needs an docstring
+#     """
 
 # MAIN
 if __name__ == "__main__":
@@ -51,6 +83,38 @@ if __name__ == "__main__":
     df = pd.read_csv(
         f"data/corpus/{args.input_file}"
     )
+
+    # Get input data into correct shape for the embedder
+    list_sentences = preprocess_corpus_for_embedder(
+        df['sentence_text'],
+        replace_abbr= {"PD": "parkinson's disease"}
+    )
+    # list_sentences = df['sentence_text'].values
+
+    # Load pre-trained model
+    if args.embedding_model == 'sbert':
+        model = SentenceTransformer('NeuML/pubmedbert-base-embeddings')
+        # model = SentenceTransformer('all-MiniLM-L6-v2')
+
+    # Apply model to encode sentences
+    # TODO: Placed limit while in testing phase
+    # list_sentences = list_sentences[:1000]
+    print(f"Start processing {list_sentences.shape[0]} sentences.")
+    embeddings = model.encode(list_sentences)
+
+    # Create `vectors` directory, if it does not yet exists.
+    Path("data/vectors/").mkdir(exist_ok = True)
+
+    # Save TODO: while building code
+    with open(f"data/vectors/{args.input_file.split('.')[0]}.pickle", "wb") as handle:
+        pickle.dump(embeddings, handle)
+    
+    # Add corpus_id to vector
+    # corpus_id consist out of the following:
+    # [paper_id, head_id, paragraph_id, sentence_id]
+    # TODO: Does it matter from which corpus it came from? (if yes, then we need to take that into account as well)
+
+    # Save as a pickle
 
 
     print("End of corpus2vector.py")
