8e25d5d6d6d4caaee3d4bf035e2b508e07b7504b
CharmingChocobo <jbeenen93@gmail.com>
2025-07-14 17:11:15 +0200

    Playing around with lemmatizers

--- scripts/corpus2vector.py
+++ scripts/corpus2vector.py
@@ -8,6 +8,11 @@
 import argparse
 from pathlib import Path
 import pandas as pd
+import numpy as np
+
+from nltk.stem import WordNetLemmatizer
+import spacy
+
 # import re
 import pickle
 
@@ -53,7 +58,7 @@ def collect_arguments() -> argparse.Namespace:
 
     return parser.parse_args()
 
-def preprocess_corpus_for_embedder(serie: pd.Series, replace_abbr : dict = None) -> list:
+def preprocess_corpus_for_embedder(serie: pd.Series, replace_abbr : dict = None) -> np.array:
     """Pre-process sentences (in pd.Series) to a list of sentences that is ready for an embedder for sentence similarity analysis. 
     """
     # Replace abbreviated words (TODO: is this necessary though?)
@@ -61,10 +66,25 @@ def preprocess_corpus_for_embedder(serie: pd.Series, replace_abbr : dict = None)
         for k in replace_abbr.keys():
             # Replace `key` (e.g. "PD") with `value` (e.g. "parkinson disease").
             serie = serie.str.replace(k, replace_abbr[k], case= True)
+
     # Make all sentence_text lower case & remove puntuations & turn `serie` (pd.Series) into a list
     list_sentences = serie.str.lower().str.strip('\[.*?\]').values
 
-    return list_sentences
+    # # TODO: decide what to do with this:
+    # # Apply lemmatize from nltk
+    # wordnet = WordNetLemmatizer()
+    # # # before lower().strip()
+    # # serie = serie.str.strip('\[.*?\]').str.split(' ').apply(lambda x: [wordnet.lemmatize(word) for word in x])
+    # # --- after lower().strip() NLTK (NOTE: lemmatizer appears to not work on words with different casing) 
+    # list_sentences = [" ".join([wordnet.lemmatize(word) for word in sent.split(' ')]) for sent in list_sentences]
+    # print("NLTK lemmatiser was active")
+    # # --- after lower().strip() SPACY
+    # nlp = spacy.load('en_core_web_sm')
+    # list_sentences = [nlp(sent) for sent in list_sentences]
+    # list_sentences = [" ".join([token.lemma_ for token in sent]) for sent in list_sentences]
+    # print("SpaCy lemmatiser was active")
+
+    return np.array(list_sentences)
 
 
 # def main():
@@ -87,20 +107,23 @@ if __name__ == "__main__":
     # Get input data into correct shape for the embedder
     list_sentences = preprocess_corpus_for_embedder(
         df['sentence_text'],
-        replace_abbr= {"PD": "parkinson's disease"}
+        replace_abbr= {"PD": "parkinson's disease"} # TODO: make this variable for the user to input?
     )
     # list_sentences = df['sentence_text'].values
 
     # Load pre-trained model
     if args.embedding_model == 'sbert':
-        model = SentenceTransformer('NeuML/pubmedbert-base-embeddings')
-        # model = SentenceTransformer('all-MiniLM-L6-v2')
+        model = SentenceTransformer('NeuML/pubmedbert-base-embeddings') # TODO: make this variable for when an new/better pretrained model becomes available
 
     # Apply model to encode sentences
-    # TODO: Placed limit while in testing phase
-    # list_sentences = list_sentences[:1000]
+    # # TODO: Placed limit while in testing phase
+    # list_sentences = list_sentences[:10]
     print(f"Start processing {list_sentences.shape[0]} sentences.")
     embeddings = model.encode(list_sentences)
+    # TODO: multiprocessing: https://github.com/UKPLab/sentence-transformers/blob/master/examples/sentence_transformer/applications/computing-embeddings/computing_embeddings_multi_gpu.py
+
+    # # TODO: Add a 'Semantic search' vector?
+    # model.encode_document(list_sentences)
 
     # Create `vectors` directory, if it does not yet exists.
     Path("data/vectors/").mkdir(exist_ok = True)
